5971cffc4d3599a029fc7e31a185420e814344c7
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-24 18:05:11 +0300

    EfProductDal Implemented from EfEntityRepositoryBase and NorthwindContext

--- Northwind.API/DataAccess/EfProductDal.cs
+++ Northwind.API/DataAccess/EfProductDal.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Northwind.API.Entities;
 
 namespace Northwind.API.DataAccess
 {
-    public class EfProductDal:IProductDal
+    public class EfProductDal: EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
     {
+       // Not need to implement
+       // Will be implemented by EfEntityRepository
     }
 }
