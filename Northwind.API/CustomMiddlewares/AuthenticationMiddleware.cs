5cb819d6e795967ad0871ac1d7133e2a1cac3380
Hidayat arghandabi <hidayatarg@gmail.com>
2018-10-08 10:59:13 +0300

    Developed middlwared

--- Northwind.API/CustomMiddlewares/AuthenticationMiddleware.cs
+++ Northwind.API/CustomMiddlewares/AuthenticationMiddleware.cs
@@ -19,49 +19,32 @@ namespace Northwind.API.CustomMiddlewares
         public async Task Invoke(HttpContext context)
         {
             string authHeader = context.Request.Headers["Authorization"];
-
-            // basic Hidayat:1234
-            // Check the auth Header
-            // There might be more than one authentications
-            if (authHeader !=null && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
+            // basic hidayat:12345
+            // 012345
+            if (authHeader != null && authHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase))
             {
-                // Username token
                 var token = authHeader.Substring(6).Trim();
-                // Convert to normal string
-                var credentialString = Encoding.UTF8.GetString
-                    (Convert.FromBase64String(token));
-                // Splitting the password from username
-                var credentials = credentialString.Split(':');
+                //Encoding to normal string
+                var credentialString = token;
+                var credential = credentialString.Split(':');
 
-                // Connecting to DB to check if the password and username is avaliable or not
-                // at top we have array of creditial with credintital[0]-username and credintial[1] password
-                if (credentials[0]=="hidayat" && credentials[1]=="1234")
+                //DB check here
+                if (credential[0] == "hidayat" && credential[1] == "12345")
                 {
-                    // Identity principal
-                    // claim: Type 
+                    //Principals
                     var claims = new[]
                     {
-                        new Claim("name", credentials[0]),
-                        // Role 
-                        // It is good if you get it from the DB
-                        //new Claim("name", credentials[0]),
+                        new Claim("name", credential[0]),
+                        // Bring from the DB
                         new Claim(ClaimTypes.Role, "Admin")
-
-
                     };
-                    // Creat Identity
-                    // ClaimsIdentity wants claims and AuthenticationType
+
                     var identity = new ClaimsIdentity(claims, "basic");
-
                     context.User = new ClaimsPrincipal(identity);
-
                 }
             }
-
-            // If there is no authHeader
             else
             {
-                // You need to authenticated to request 
                 context.Response.StatusCode = 401;
             }
 
